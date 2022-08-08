3324ee30b22fd5c87da21d1ab2af1b8372d20292
vipulradon <you@example.com>
2022-08-08 12:48:33 +0530

    lodash assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const lodash = require("lodash");
 const abc = require('../introduction/intro')
 const router = express.Router();
 const logg  = require("../logger/logger")
 const util = require("../util/helper")
 const formatter = require("../validator/formatter")
+
+let months = ["january","february","march","april","may","june","july","august","september","october","november","december"]
+
+
+let oddNumbers = [1,3,5,7,9,11,13,15,17,19];
+
+let array1 = [1,1,2,5];
+let array2 = [1,7,2,10];
+let array3 = [3,6,8,100];
+let array4 = [1,55,12,10];
+let array5 = [1,1,9,5];
 
 
 router.get('/test-me', function (req, res) {
@@ -19,7 +31,14 @@ router.get('/test-me', function (req, res) {
 formatter.trim(organisation);
 formatter.changetoLowerCase(name);
 formatter.changetoUpperCase(name);
-
+let subArrays = lodash.chunk(months,[size=3])
+console.log(subArrays);
+let Tail = lodash.tail(oddNumbers);
+console.log(Tail);
+let Union = lodash.union(array1,array2,array3,array4,array5);
+console.log(Union);
+let pairsObject = lodash.fromPairs([['horror','The Shining'],['drama','Titanic'],['thriller','Shutter Island'],['fantasy','Pans Labyrinth']])
+console.log(pairsObject)
     res.send('My second ever api!')
 });
 
