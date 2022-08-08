970e2fc9189136f7eb7a9f2a80222f4866d87e6b
vipulradon <you@example.com>
2022-08-08 17:52:16 +0530

    getapiAssignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -10,12 +10,12 @@ router.get('/test-me', function (req, res) {
     res.send('My second ever api!')
 });
 
-router.get('/students', function (req, res){
+router.get('/students', function (req, res) {
     let students = ['Sabiha', 'Neha', 'Akash']
     res.send(students)
 })
 
-router.get('/student-details/:batch', function(req, res){
+router.get('/student-details/:batch', function (req, res) {
     /*
     params is an attribute inside request that contains 
     dynamic values.
@@ -28,16 +28,59 @@ router.get('/student-details/:batch', function(req, res){
 
     // JSON strigify function helps to print an entire object
     // We can use any ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
+    console.log("This is the request " + JSON.stringify(requestParams))
     let studentName = requestParams.batch
     console.log('Name of the students batch is ', studentName)
-  
+
     res.send('Dummy response')
 })
+// Assignment 1 =======================================================================================================
+let movies = ["Rang de basanti", "The shining", "Lord of the ring", "Batman begins"]
 
-router.get("/organisation/:name",function(req,res){
-   res.send(req.params.name + " is located at Bangalore") 
+
+router.get("/movies", function (req, res) {
+    res.send(movies);
 })
+// Assignment 2 && 3 ======================================================
+router.get("/movies/:indexNumber", function (req, res) {
+    if (req.params.indexNumber < movies.length && req.params.indexNumber >= 0) {
+        res.send(movies[req.params.indexNumber])
+    }else {
+        res.send("Please input a valid indexNumber")
+    }
+})
+// Assignment 4 =========================================================================
+moviesArray =[ {
+    "id": 1,
+    "name": "The Shining"
+   }, {
+    "id": 2,
+    "name": "Incendies"
+   }, {
+    "id": 3,
+    "name": "Rang de Basanti"
+   }, {
+    "id": 4,
+    "name": "Finding Nemo"
+   }]
+   
+router.get("/films",function(req,res){
+res.send(moviesArray)
+
+})
+
+//Assignment 5 ==================================================================================
+router.get("/films/:filmId",function(req,res){
+    if (req.params.filmId <= moviesArray.length && req.params.filmId > 0){
+        res.send(moviesArray[req.params.filmId - 1]);
+
+
+}else {
+    res.send("No movie exists with this id")
+   
+} })
+
+
 
 
 module.exports = router;
