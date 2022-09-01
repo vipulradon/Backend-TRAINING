fed3a462b598489653f19a6c114142042ac39cd8
vipulradon <you@example.com>
2022-09-01 15:37:18 +0530

    assignmentAxios

--- src/routes/route.js
+++ src/routes/route.js
@@ -12,7 +12,7 @@ router.get("/test-me", function (req, res) {
 router.get("/cowin/states", CowinController.getStates)
 router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
 router.get("/cowin/getByPin", CowinController.getByPin)
-
+router.get("/cowin/getVaccinesInDistrict",CowinController.getVaccineInDistrict)
 router.post("/cowin/getOtp", CowinController.getOtp)
 
 // WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
